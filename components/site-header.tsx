0b30c8726c86340a3a84d53fcb8ebbb2df037d1f
CodeAsDev <codeas.dev@gmail.com>
2024-04-10 22:40:56 +0800

    feat: Finishing SiteHeader and nav for desktop

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,52 @@
+import { siteConfig } from '@/config/site'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { Icons } from './icons'
+import MainNav from './main-nav'
+
 function SiteHeader() {
-  return <header>Hi</header>
+  return (
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex items-center h-14 max-w-screen-2xl">
+        <MainNav />
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="flex items-center">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: 'ghost' }),
+                  'w-10 px-0',
+                )}
+              >
+                <Icons.gitHub className="size-4" />
+                <span className="sr-only">Github</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: 'ghost' }),
+                  'w-10 px-0',
+                )}
+              >
+                <Icons.twitter className="size-4" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default SiteHeader
