f8d9561dd808407d346fb961a68458fcaf75148d
CodeAsDev <codeas.dev@gmail.com>
2024-04-16 23:18:18 +0800

    fix: z-index of header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,37 +9,19 @@ import ModeToggle from './mode-toggle'
 
 function SiteHeader() {
   return (
-    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex items-center h-14 max-w-screen-2xl">
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({ variant: 'ghost' }),
-                  'w-10 px-0 hidden sm:inline-flex',
-                )}
-              >
+            <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+              <div className={cn(buttonVariants({ variant: 'ghost' }), 'w-10 px-0 hidden sm:inline-flex')}>
                 <Icons.gitHub className="size-4" />
                 <span className="sr-only">Github</span>
               </div>
             </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({ variant: 'ghost' }),
-                  'w-10 px-0 hidden sm:inline-flex',
-                )}
-              >
+            <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
+              <div className={cn(buttonVariants({ variant: 'ghost' }), 'w-10 px-0 hidden sm:inline-flex')}>
                 <Icons.twitter className="size-4" />
                 <span className="sr-only">Twitter</span>
               </div>
