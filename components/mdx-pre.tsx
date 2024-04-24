3b31a8caf2394e911c786e3a672181c247dd4adb
CodeAsDev <codeas.dev@gmail.com>
2024-04-24 09:09:38 +0800

    fix: pre scroll

--- components/mdx-pre.tsx
+++ components/mdx-pre.tsx
@@ -11,16 +11,18 @@ function Pre({ children, raw, filename, ...props }: MdxPreProps) {
   const lang = props['data-language']
 
   return (
-    <pre {...props} className="pt-0">
-      <div className="mb-3 h-10 flex justify-between items-center py-2 px-4 text-sm bg-gray-900">
+    <>
+      <div className="rounded-t-md w-full h-10 flex justify-between items-center py-2 px-4 text-sm bg-gray-900">
         <span className="uppercase">{lang ?? null} </span>
         <span className="flex items-center">
-          {filename ? <span className="italic mr-4 text-green-700 dark:text-green-900">{filename}</span> : null}
+          {filename ? <span className="italic mr-3 text-green-700 dark:text-green-900">{filename}</span> : null}
           <CopyButton text={raw} />
         </span>
       </div>
-      {children}
-    </pre>
+      <pre {...props} className="rounded-t-none">
+        {children}
+      </pre>
+    </>
   )
 }
 
