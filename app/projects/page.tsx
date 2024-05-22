47db801dbd49911acec2adf3fceeba53cbefd1b1
CodeAsDev <codeas.dev@gmail.com>
2024-05-22 11:16:39 +0800

    fix: wrong text

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 function Projects() {
   return (
     <section className="container text-center my-10 max-w-6xl">
-      <h1>Comming Son...</h1>
+      <h1>Comming Soon...</h1>
     </section>
   )
 }
